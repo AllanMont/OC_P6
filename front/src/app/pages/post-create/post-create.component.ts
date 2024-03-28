fb26b420f1c7275a834bfcf7576c3d40e822cc1c
AllanMont <allan.mont34@gmail.com>
2024-03-28 03:24:29 +0100

    feat(PostCreatePage)New Page for create post

--- front/src/app/pages/post-create/post-create.component.ts
+++ front/src/app/pages/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostCreateComponent implements OnInit {
 
+  registerForm: FormGroup = new FormGroup({
+    subject: new FormControl('', Validators.required),
+    titlePost: new FormControl('', Validators.required),
+    contentPost: new FormControl('', Validators.required)
+  });
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Formulaire valide, soumission en cours...');
+    } else {
+      console.log('Formulaire invalide, veuillez vérifier les champs.');
+    }
+  }
 }
