22615f79ce0f9a1d99054c66085bd601dd6bbbc2
AllanMont <allan.mont34@gmail.com>
2024-03-28 03:36:14 +0100

    feat(ProfilePage)New Profile Page

--- front/src/app/pages/profile/profile.component.ts
+++ front/src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  registerForm: FormGroup = new FormGroup({
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Formulaire valide, soumission en cours...');
+    } else {
+      console.log('Formulaire invalide, veuillez vérifier les champs.');
+    }
+  }
+
+  onDisconnect(){
+    console.log('Déconnexion en cours...');
+  }
 }
