8d8acca4412a0d06334e428a77ead36184b392d2
AllanMont <allan.mont34@gmail.com>
2024-04-05 00:33:52 +0200

    feat(BDD)Spring read schema.sql to create BDD

---
+++ back/src/main/resources/schema.sql
@@ -0,0 +1,48 @@
+DROP DATABASE IF EXISTS OC_P6;
+CREATE DATABASE OC_P6;
+USE OC_P6;
+
+CREATE TABLE `users` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP
+);
+
+CREATE TABLE `topics` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP
+);
+
+CREATE TABLE `posts` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    subject_id INT,
+    author_id INT,
+    created_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (subject_id) REFERENCES topics(id),
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE `commments` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL,
+    article_id INT,
+    author_id INT,
+    created_at TIMESTAMP,
+    FOREIGN KEY (article_id) REFERENCES posts(id),
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE `subscriptions` (
+    user_id INT,
+    subject_id INT,
+    created_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (user_id, subject_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (subject_id) REFERENCES topics(id)
+);
