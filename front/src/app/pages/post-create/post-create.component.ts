1b38f1ebc3723181fcf26094f74e02b874008387
AllanMont <allan.mont34@gmail.com>
2024-05-18 17:21:28 +0200

    feat(Post)Can create post

--- front/src/app/pages/post-create/post-create.component.ts
+++ front/src/app/pages/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PostService } from 'src/app/core/services/post.service';
+import { TopicService } from 'src/app/core/services/topic.service';
 
 @Component({
   selector: 'app-post-create',
@@ -11,23 +12,38 @@ export class PostCreateComponent implements OnInit {
 
   postForm: FormGroup | any;
   message: string = '';
+  topics: any[] = [];
 
-  constructor(private formBuilder: FormBuilder, private postService: PostService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private postService: PostService,
+    private topicService: TopicService
+  ) { }
 
   ngOnInit(): void {
     this.postForm = this.formBuilder.group({
-      subject: ['', Validators.required],
+      selectedTopic: [null, Validators.required],
       titlePost: ['', Validators.required],
       contentPost: ['', Validators.required]
+    });
+
+    this.loadTopics();
+  }
+
+  loadTopics(): void {
+    this.topicService.getAllTopics().subscribe((data: any) => {
+      this.topics = data;
+    }, error => {
+      console.error('Erreur lors du chargement des topics', error);
     });
   }
 
   onSubmit() {
     if (this.postForm.valid) {
       const post = {
-        subject: this.postForm.value.subject,
         title: this.postForm.value.titlePost,
-        content: this.postForm.value.contentPost
+        content: this.postForm.value.contentPost,
+        topicId: this.postForm.value.selectedTopic
       };
       this.postService.createPost(post).subscribe(
         response => {
